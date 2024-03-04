505c90c6e5657c3904089a09cfda87acfda036e8
paulgar21 <psgarcia@puce.edu.ec>
2024-03-03 21:37:59 -0500

    Final-EquipInCheckAPI

---
+++ EquipInCheckAPI/src/main/java/com/puce/EquipInCheck/model/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.puce.EquipInCheck.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.puce.EquipInCheck.model.entity.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+    Cliente findByRuc(String ruc);
+    
+}
